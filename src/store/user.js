895f7da35ab07abb61738c0246e4c726775f7853
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-02-05 13:39:29 +0700

    added signout action

--- src/store/user.js
+++ src/store/user.js
@@ -30,5 +30,10 @@ export default defineStore("user", {
 
       this.userLoggedIn = true;
     },
+    async signOut() {
+      await auth.signOut();
+
+      this.userLoggedIn = false;
+    },
   },
 });
