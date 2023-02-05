2422d1c83cef75b1ee23f479e143b13c8b530e17
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-02-05 13:38:10 +0700

    added redirect page after attempt auth/register

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -78,7 +78,7 @@ export default {
 
       this.login_alert_variant = "bg-green-500";
       this.login_alert_msg = "You are successfully logged in.";
-      console.log(values);
+      window.location.reload();
     },
   },
 };
